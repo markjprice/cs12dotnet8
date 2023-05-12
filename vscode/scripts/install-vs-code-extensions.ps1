5d55ae2abccc03e56ace2b315143c00604182296
Mark J Price <markjprice@gmail.com>
2023-05-12 12:45:27 +0100

    Add VSCode extension scripts

---
+++ vscode/scripts/install-vs-code-extensions.ps1
@@ -0,0 +1,5 @@
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.dotnet-interactive-vscode
+code --install-extension tintoy.msbuild-project-tools
+code --install-extension humao.rest-client
+code --install-extension icsharpcode.ilspy-vscode
---
+++ vscode/scripts/uninstall-vs-code-extensions.ps1
@@ -0,0 +1,5 @@
+code --uninstall-extension ms-dotnettools.csharp
+code --uninstall-extension ms-dotnettools.dotnet-interactive-vscode
+code --uninstall-extension tintoy.msbuild-project-tools
+code --uninstall-extension humao.rest-client
+code --uninstall-extension icsharpcode.ilspy-vscode
