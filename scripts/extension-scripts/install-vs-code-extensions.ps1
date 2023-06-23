fa3df5bd4f8a6a09278df66c67e64b871c3d26b0
Mark J Price <markjprice@gmail.com>
2023-06-23 11:24:20 +0100

    Add scripts folder

--- scripts/extension-scripts/install-vs-code-extensions.ps1
+++ scripts/extension-scripts/install-vs-code-extensions.ps1
@@ -1,4 +1,4 @@
-code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.csdevkit
 code --install-extension ms-dotnettools.dotnet-interactive-vscode
 code --install-extension tintoy.msbuild-project-tools
 code --install-extension humao.rest-client
--- scripts/extension-scripts/uninstall-vs-code-extensions.ps1
+++ scripts/extension-scripts/uninstall-vs-code-extensions.ps1
@@ -1,4 +1,6 @@
+code --uninstall-extension ms-dotnettools.csdevkit
 code --uninstall-extension ms-dotnettools.csharp
+code --uninstall-extension ms-dotnettools.vscodeintellicode-csharp
 code --uninstall-extension ms-dotnettools.dotnet-interactive-vscode
 code --uninstall-extension tintoy.msbuild-project-tools
 code --uninstall-extension humao.rest-client
