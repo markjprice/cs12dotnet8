74c31efcceaec26e066d574508a1ecf54ac932b0
Mark J Price <markjprice@gmail.com>
2023-06-25 14:33:27 +0100

    Add solution code for Ch1

---
+++ code/Chapter01/AboutMyEnvironment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AboutMyEnvironment
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Environment.CurrentDirectory);
+      Console.WriteLine(Environment.OSVersion.VersionString);
+      Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+    }
+  }
+}
---
+++ code/Chapter01/HelloCS/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C#!");
+
+string name = typeof(Program).Namespace ?? "None!";
+Console.WriteLine($"Namespace: {name}");
+
+throw new Exception();
+int z;
