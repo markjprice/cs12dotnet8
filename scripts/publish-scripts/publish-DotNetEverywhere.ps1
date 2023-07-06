10b37c972685231702790d028743837149ebf31e
Mark J Price <markjprice@gmail.com>
2023-07-06 17:32:07 +0100

    Create publish-DotNetEverywhere.ps1

---
+++ scripts/publish-scripts/publish-DotNetEverywhere.ps1
@@ -0,0 +1,5 @@
+dotnet publish -c Release -r win10-x64 --self-contained
+dotnet publish -c Release -r osx-x64 --self-contained
+dotnet publish -c Release -r osx.11.0-arm64 --self-contained
+dotnet publish -c Release -r linux-x64 --self-contained
+dotnet publish -c Release -r linux-arm64 --self-contained
