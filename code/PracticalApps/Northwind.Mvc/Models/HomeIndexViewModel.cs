f9cd43320bd63b4d587827616c4e8ad38e7e6fd6
Mark J Price <markjprice@gmail.com>
2023-09-04 13:41:04 +0100

    Ch16 revisions

--- code/PracticalApps/Northwind.Mvc/Models/HomeIndexViewModel.cs
+++ code/PracticalApps/Northwind.Mvc/Models/HomeIndexViewModel.cs
@@ -2,9 +2,5 @@
 
 namespace Northwind.Mvc.Models;
 
-public record HomeIndexViewModel
-(
-  int VisitorCount,
-  IList<Category> Categories,
-  IList<Product> Products
-);
+public record HomeIndexViewModel(int VisitorCount,
+  IList<Category> Categories, IList<Product> Products);
