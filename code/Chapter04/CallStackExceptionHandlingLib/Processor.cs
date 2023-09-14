8f2ee9c629053eef2c7e8e217bdf46c12e014ae0
Mark J Price <markjprice@gmail.com>
2023-09-14 11:19:15 +0100

    Update Processor.cs

--- code/Chapter04/CallStackExceptionHandlingLib/Processor.cs
+++ code/Chapter04/CallStackExceptionHandlingLib/Processor.cs
@@ -1,19 +1,18 @@
 ﻿using static System.Console;
 
-namespace CallStackExceptionHandlingLib
-{
-  public class Processor
-  {
-    public static void Gamma() // public so it can be called from outside.
-    {
-      WriteLine("In Gamma");
-      Delta();
-    }
+namespace CallStackExceptionHandlingLib;
 
-    private static void Delta() // private so it can only be called internally.
-    {
-      WriteLine("In Delta");
-      File.OpenText("bad file path");
-    }
+public class Processor
+{
+  public static void Gamma() // public so it can be called from outside.
+  {
+    WriteLine("In Gamma");
+    Delta();
+  }
+
+  private static void Delta() // private so it can only be called internally.
+  {
+    WriteLine("In Delta");
+    File.OpenText("bad file path");
   }
 }
