728b4f2b9ab511d2de8e03b4b6458f2270e2d2e8
Mark J Price <markjprice@gmail.com>
2023-09-14 14:07:50 +0100

    FD revisions for Ch6

--- code/Chapter06/PacktLibrary/Mutability.cs
+++ code/Chapter06/PacktLibrary/Mutability.cs
@@ -1,22 +1,22 @@
 ﻿namespace Packt.Shared;
 
 // A mutable record class.
-record class C1
+public record class C1
 {
   public string? Name { get; set; }
 }
 
 // An immutable record class.
-record class C2(string? Name);
+public record class C2(string? Name);
 
 // A mutable record struct.
-record struct S1
+public record struct S1
 {
   public string? Name { get; set; }
 }
 
 // Another mutable record struct.
-record struct S2(string? Name);
+public record struct S2(string? Name);
 
 // An immutable record struct.
-readonly record struct S3(string? Name);
+public readonly record struct S3(string? Name);
