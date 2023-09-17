daf86c5029f8d3eee8f7e742dd5b6fe31b2813fa
Mark J Price <markjprice@gmail.com>
2023-09-17 12:22:52 +0100

    FD revisions for Ch9

---
+++ code/Chapter09/WorkingWithEnvVars/Program.Helpers.cs
@@ -0,0 +1,27 @@
+﻿// null namespace to merge with auto-generated Program.
+
+partial class Program
+{
+  private static void SectionTitle(string title)
+  {
+    ConsoleColor previousColor = ForegroundColor;
+    // Use a color that stands out on your system.
+    ForegroundColor = ConsoleColor.DarkYellow;
+    WriteLine($"*** {title} ***");
+    ForegroundColor = previousColor;
+  }
+
+  private static void DictionaryToTable(IDictionary dictionary)
+  {
+    Table table = new();
+    table.AddColumn("Key");
+    table.AddColumn("Value");
+
+    foreach (string key in dictionary.Keys)
+    {
+      table.AddRow(key, dictionary[key]!.ToString()!);
+    }
+
+    AnsiConsole.Write(table);
+  }
+}
---
+++ code/Chapter09/WorkingWithEnvVars/Program.cs
@@ -0,0 +1,48 @@
+﻿#region Reading all environment variables
+
+SectionTitle("Reading all environment variables for process");
+IDictionary vars = GetEnvironmentVariables();
+DictionaryToTable(vars);
+
+SectionTitle("Reading all environment variables for machine");
+IDictionary varsMachine = GetEnvironmentVariables(
+  EnvironmentVariableTarget.Machine);
+DictionaryToTable(varsMachine);
+
+SectionTitle("Reading all environment variables for user");
+IDictionary varsUser = GetEnvironmentVariables(
+  EnvironmentVariableTarget.User);
+DictionaryToTable(varsUser);
+
+#endregion
+
+#region Expanding environment variables
+
+string myComputer = "My username is %USERNAME%. My CPU is %PROCESSOR_IDENTIFIER%.";
+
+WriteLine(ExpandEnvironmentVariables(myComputer));
+
+#endregion
+
+#region Setting and getting an environment variable
+
+string password_key = "MY_PASSWORD";
+
+SetEnvironmentVariable(password_key, "Pa$$w0rd");
+
+string? password = GetEnvironmentVariable(password_key);
+
+WriteLine($"{password_key}: {password}");
+
+string secret_key = "MY_SECRET";
+
+string? secret = GetEnvironmentVariable(secret_key, EnvironmentVariableTarget.Process);
+WriteLine($"Process - {secret_key}: {secret}");
+
+secret = GetEnvironmentVariable(secret_key, EnvironmentVariableTarget.Machine);
+WriteLine($"Machine - {secret_key}: {secret}");
+
+secret = GetEnvironmentVariable(secret_key, EnvironmentVariableTarget.User);
+WriteLine($"User    - {secret_key}: {secret}");
+
+#endregion
