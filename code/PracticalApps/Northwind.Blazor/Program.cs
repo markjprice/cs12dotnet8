00544a7a7b8576b7596770dea125d3399f831fc7
Mark J Price <markjprice@gmail.com>
2023-10-12 11:35:19 +0100

    Updated for RC2

--- code/PracticalApps/Northwind.Blazor/Program.cs
+++ code/PracticalApps/Northwind.Blazor/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-  .AddServerComponents()
-  .AddWebAssemblyComponents();
+  .AddInteractiveServerComponents()
+  .AddInteractiveWebAssemblyComponents();
 builder.Services.AddNorthwindContext();
 builder.Services.AddTransient<INorthwindService,
   NorthwindServiceServerSide>();
@@ -16,7 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Error");
+  app.UseExceptionHandler("/Error", createScopeForErrors: true);
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
 }
@@ -24,9 +24,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-  .AddServerRenderMode()
-  .AddWebAssemblyRenderMode();
+  .AddInteractiveServerRenderMode()
+  .AddInteractiveWebAssemblyRenderMode();
 
 app.Run();
